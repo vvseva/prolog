3cd6f0e7ffec7c70ad53ad5cec286ab6f069d54c
Vsevolod Suschevskiy <50490206+vvseva@users.noreply.github.com>
2019-09-17 17:04:04 +0200

    Update lab4.pl

--- exercises/lab4.pl
+++ exercises/lab4.pl
@@ -25,3 +25,21 @@ in(X, Y) :-
     directlyIn(X, Z), in(Z, Y).
 
 % trains program
+directTrain(saarbruecken,dudweiler).
+directTrain(forbach,saarbruecken).
+directTrain(freyming,forbach).
+directTrain(stAvold,freyming).
+directTrain(fahlquemont,stAvold).
+directTrain(metz,fahlquemont).
+directTrain(nancy,metz). 
+
+travelFromTo(X, Y) :-
+    directTrain(X, Y).
+    
+travelFromTo(X, Y) :-
+    directTrain(Y, X).
+    
+travelFromTo(X, Y) :-
+    directTrain(X, Z), travelFromTo(Z, Y).
+
+% travelFromTo(nancy,saarbruecken).
