a1eea65bac5d470f31d0250b1ac12c4b6afb8a1a
Vsevolod Suschevskiy <50490206+vvseva@users.noreply.github.com>
2019-09-17 17:01:28 +0200

    Update lab4.pl

--- exercises/lab4.pl
+++ exercises/lab4.pl
@@ -3,7 +3,7 @@
 :- set_prolog_stack(global, limit(8 000 000)).  % limit term space (8Mb)
 :- set_prolog_stack(local,  limit(2 000 000)).  % limit environment space
 
-% Your program goes here
+% clock program
 clockSequence(tick(nothing)).
 clockSequence(tock(nothing)).
 
@@ -12,3 +12,16 @@ clockSequence(tick(tock(S))) :- clockSequence(tock(S)).
 clockSequence(tock(tick(S))) :- clockSequence(tick(S)).
 
 % tick(tock(tick(nothing)))
+
+% Russian doll, matreshca program
+directlyIn(natasha, irina).
+directlyIn(olga, natasha).
+directlyIn(katerina, olga).
+
+in(X, Y) :-
+    directlyIn(X, Y).
+
+in(X, Y) :-
+    directlyIn(X, Z), in(Z, Y).
+
+% trains program
