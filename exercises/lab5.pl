2160cea2ebee212cc6ff7611afd17bf2dbdc96e6
Vsevolod Suschevskiy <50490206+vvseva@users.noreply.github.com>
2019-09-24 16:59:40 +0200

    Update lab5.pl

--- exercises/lab5.pl
+++ exercises/lab5.pl
@@ -14,3 +14,20 @@ c(2).
 *   function stups after cut, wich is it is not going to check second element, condition
 *
 */
+
+%% split list into positive and negative
+
+split([], [], []).
+/*
+ * [] is an empty list 
+ * function takes three lists
+ */
+
+split([H|T], [H|P], N) :-
+    H >= 0,
+    !,
+    split(T, P, N).
+
+split([H|T], P, [H|N]) :-
+    H < 0,
+    split(T, P, N).
